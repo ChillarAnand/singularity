0ddf299461ab6fa05b6ce75b51f891f105a6dc24
anand <anand21nanda@gmail.com>
2015-07-15 12:09:48 +0530

    Updated core

--- singularity.el
+++ singularity.el
@@ -1,57 +1,58 @@
-;;; singularity.el --- 
-;; 
+;;; singularity.el ---
+;;
 ;; Filename: singularity.el
-;; Description: 
-;; Author: Anand
-;; Maintainer: 
+;; Description: Singularity is a  function in emacs, which is capable of doing anything! 
+;; Author: ChillarAnand(Anand Reddy Pandikunta)
+;; Maintainer: ChillarAnand(Anand Reddy Pandikunta)
 ;; Created: Wed Jul  8 17:33:36 2015 (+0530)
-;; Version: 
-;; Package-Requires: ()
-;; Last-Updated: 
-;;           By: 
+;; Version: 0.1
+;; Package-Requires: ((helm "1.7.5") (helm-chrome "1.131226") (helm-github-stars "1.3.2") (helm-dired-recent-dirs "0.1") (key-chord "0.6"))
+
+;; Last-Updated:
+;;           By:
 ;;     Update #: 0
-;; URL: 
-;; Doc URL: 
-;; Keywords: 
-;; Compatibility: 
-;; 
+;; URL: https://github.com/ChillarAnand/singularity
+;; Doc URL:
+;; Keywords: helm convinience 
+;; Compatibility:
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;;; Commentary: 
-;; 
-;; 
-;; 
+;;
+;;; Commentary:
+;;
+;;
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Change Log:
-;; 
-;; 
+;;
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or (at
 ;; your option) any later version.
-;; 
+;;
 ;; This program is distributed in the hope that it will be useful, but
 ;; WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;; General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
-;; 
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Code:
 
 (require 'helm)
-(require 'helm-files)
-(require 'helm-dired)
-(require 'helm-dired-recent-dirs)
 (require 'helm-chrome)
-(require 'helm-descbinds)
+;; (require 'helm-descbinds)
 (require 'helm-github-stars)
+(require 'helm-dired-recent-dirs)
+(require 'key-chord)
+
 
 (defvar singularity-github-username nil)
 
@@ -60,11 +61,12 @@
     :candidates (lambda ()
                   (let ((cmds))
                     (mapatoms
-                     (lambda (elt) (when (commandp elt) (push elt cmds))))
+                     (lambda (elt)
+                       (when (commandp elt) (push elt cmds))))
                     cmds))
     :coerce #'intern-soft
     :action #'command-execute)
-  "A simple helm source for Emacs commands.")
+  "Helm source for Emacs commands.")
 
 (defvar singularity-source-emacs-commands-history
   (helm-build-sync-source "Emacs commands history"
@@ -75,9 +77,9 @@
                     cmds))
     :coerce #'intern-soft
     :action #'command-execute)
-  "Emacs commands history")
+  "Helm source for Emacs commands history")
 
-(defvar singularity-sources '(singularity-source-buffers-list
+(defvar singularity-sources '(helm-source-buffers-list
                               helm-source-recentf
                               helm-source-dired-recent-dirs
                               singularity-source-emacs-commands-history
@@ -86,27 +88,44 @@
                               hgs/helm-c-source-stars
                               hgs/helm-c-source-repos
                               helm-source-buffer-not-found
-                              hgs/helm-c-source-search))
+                              hgs/helm-c-source-search)) 
+
+(defvar singularity-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-c C-s") 'singularity)
+    (key-chord-define-global "fj" 'singularity)
+    map)
+  "Key map for Singularity")
 
 
-(defun singularity-make-source-buffers-list ()
-  (setq singularity-source-buffers-list
-        (helm-make-source "Buffers" 'helm-source-buffers)))
+;;;;;;;;;;;;;;;;;
+;;; Singularity
 
 (defun singularity ()
   ""
   (interactive)
-  (unless singularity-source-buffers-list
-    (singularity-make-source-buffers-list))
-  
   (let ((helm-ff-transformer-show-only-basename nil))
     (helm :sources singularity-sources
           :buffer "*helm mini*"
           :truncate-lines t)))
 
 
+;;;###autoload
+(define-minor-mode singularity-mode
+  "Singularity"
+  nil
+  " S "
+  singularity-mode-map)
 
 
+(defun singularity-enable ()
+  "Enable `singularity-mode`'"
+  (singularity-mode 1))
+
+
+(defun singularity-disable ()
+  "Disable `singularity-mode`'"
+  (singularity-mode 0))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
